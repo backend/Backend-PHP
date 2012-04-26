906d92f8bc08d84170064b1e93ed4989f3befd3b
Jurgens du Toit <jrgns@jrgns.net>
2012-04-26 08:36:24 +0200

    Added home template for Render

---
+++ templates/home.tpl.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo $applicationName ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- Le styles -->
+        <!--<link rel="stylesheet" href="<?php echo SITE_PATH ?>css/bootstrap.css">-->
+        <!-- From http://bootswatch.com/ -->
+        <link rel="stylesheet" href="<?php echo SITE_PATH ?>css/cerulean.min.css">
+        <link rel="stylesheet" href="<?php echo SITE_PATH ?>css/blueprint/typography.css">
+        <link rel="stylesheet" href="<?php echo SITE_PATH ?>css/blueprint/forms.css">
+        <style>
+        body {
+            padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+        }
+        </style>
+
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- Le fav and touch icons -->
+        <link rel="shortcut icon" href="images/favicon.ico">
+        <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+    </head>
+    <body>
+        <div class="container">
+            <h1>This is Home</h1>
+
+            <p>The result was</p>
+                <pre><?php echo $result ?></pre>
+        </div>
+    </body>
+</html>
