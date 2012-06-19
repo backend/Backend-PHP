7f410b843fc4bda78003761657eaefe00b1121a3
Jurgens du Toit <jurgens@prefix.co.za>
2012-06-19 08:41:56 +0200

    Tweaks for Core refactor

---
+++ templates/index.tpl.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo empty($applicationName) ? 'Backend-PHP' : $applicationName ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- Le styles -->
+        <!--<link rel="stylesheet" href="<?php echo SITE_PATH ?>css/bootstrap.css">-->
+        <!-- From http://bootswatch.com/ -->
+        <link rel="stylesheet" href="<?php echo SITE_PATH ?>css/cerulean.min.css">
+        <link rel="stylesheet" href="<?php echo SITE_PATH ?>css/blueprint/typography.css">
+        <link rel="stylesheet" href="<?php echo SITE_PATH ?>css/blueprint/forms.css">
+        <style>
+        body {
+            padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+        }
+        </style>
+
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- Le fav and touch icons -->
+        <link rel="shortcut icon" href="images/favicon.ico">
+        <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+    </head>
+    <body>
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="#"><?php echo empty($applicationName) ? 'Backend-PHP' : $applicationName ?></a>
+                    <div class="nav-collapse">
+                        <ul class="nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <h1>Backend-PHP</h1>
+
+            <p>This is the default page.</p>
+        </div>
+    </body>
+</html>
